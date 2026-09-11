127526fe83a5ab5cdbdb8e5357489c4c7d9ffc6b
karissaforlenza1-ctrl <karissaforlenza1@gmail.com>
2026-09-11 17:53:18 -0400

    Add app as shared source for automatic deployments

---
+++ app/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'msa-fbla-app-v2';
+const CACHE_FILES = ['./', './index.html', './styles.css', './manifest.json', './icon.svg', './FL_FBLA_logo.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      const responseCopy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseCopy));
+      return response;
+    }).catch(() => caches.match(event.request))
+  );
+});
